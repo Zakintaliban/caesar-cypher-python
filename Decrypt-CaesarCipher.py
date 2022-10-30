af12bc0526a34039de428446cd734f290b4df3e9
joanneeldy <95136347+joanneeldy@users.noreply.github.com>
2022-10-30 15:05:19 +0700

    Update Decrypt-CaesarCipher.py

--- Decrypt-CaesarCipher.py
+++ Decrypt-CaesarCipher.py
@@ -1,7 +1,7 @@
 import string
 
 # input kata untuk di encrypt dan menjadi lower text
-plain_text = (input("Enter the plain text: ").lower())
+encrypted = (input("Enter the encrypted text: ").lower())
 # masukan berapa angka untuk diputar
 shift = int(input("Enter the shift: "))
 # karena alphabet hanya terdapat 26 huruf maka kita gunakan modulus
@@ -9,17 +9,10 @@ shift %= 26
 
 # membuat list of alphabets
 alphabets = string.ascii_lowercase
-# membuat list pf alphabets diputar jadi contoh, abcdefghijklmnopqrstuvwxyz di beri shift 3, menjadi defghijklmnopqrstuvwxyzabc
-shifted = alphabets[shift:] + alphabets[:shift]
-# guna maketrans ialah mengganti huruf yang di input dengan huruf yang sudah diputar (contoh, a menjadi d)
-tableshift = str.maketrans(alphabets, shifted)
-# mengksekusikan plain/decrypt text menjadi shifted/encrypt text
-encrypted = plain_text.translate(tableshift)
-# hasil encrypt
-print(encrypted)
 
 # kebalikan dari shifted, mereka mundur, dari defghijklmnopqrstuvwxyzabc menjadi abcdefghijklmnopqrstuvwxyz ketika dimundur 3
 unshifted = alphabets[-shift:] + alphabets[:-shift]
+# guna maketrans ialah mengganti huruf yang ter-encrypted dengan huruf yang akan dimundur (contoh, d menjadi a)
 tableunshift = str.maketrans(alphabets, unshifted)
 # mengksekusikan shifted/encrypt text menjadi plain/decrypt text
 decrypted = encrypted.translate(tableunshift)
